c45da59808734138a9eca5039bb85abe8b31c757
nelly476 <nellybulatova12@gmail.com>
2023-03-10 12:26:41 +0300

    add status select tag

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
 const bookForm = document.getElementById("book-form");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
-const isRead = document.getElementById("status");
+const status = document.getElementById("status");
 const bookList = document.getElementById("book-list");
+let booksFromStorage = JSON.parse(localStorage.getItem("allBooks"));
 
 let myLibrary = [];
+// localStorage.clear();
 
-const bookFactory = (title, author, isRead) => {
+if (booksFromStorage) {
+  booksFromStorage.forEach((item) => {
+    // console.log(item);
+    displayBooks(item);
+  });
+  myLibrary = booksFromStorage;
+}
+
+const bookFactory = (title, author, status) => {
+  let isRead = false;
+  if (status === "Read") {
+    isRead = true;
+  }
   return { title, author, isRead };
 };
 
 bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const newBook = bookFactory(title.value, author.value, isRead.value);
+  const newBook = bookFactory(title.value, author.value, status.value);
+  // console.log(newBook);
   myLibrary.push(newBook);
+  title.value = "";
+  author.value = "";
 
-  if (myLibrary.length > 0) {
-    myLibrary.map((item) => {
-      // console.log(item);
-      const row = document.createElement("tr");
-      const titleCell = document.createElement("td");
-      const authorCell = document.createElement("td");
-
-      const titleTextCell = document.createTextNode(item.title);
-      const authorTextCell = document.createTextNode(item.author);
-      titleCell.appendChild(titleTextCell);
-      authorCell.appendChild(authorTextCell);
-      row.appendChild(titleCell);
-      row.appendChild(authorCell);
-      bookList.appendChild(row);
-    });
-  }
+  localStorage.setItem("allBooks", JSON.stringify(myLibrary));
+  displayBooks(newBook);
 });
+
+function displayBooks(newBook) {
+  const row = document.createElement("tr");
+  const titleCell = document.createElement("td");
+  const authorCell = document.createElement("td");
+  const select = document.createElement("select");
+  const option = document.createElement("option");
+  const optionTwo = document.createElement("option");
+
+  if (newBook.isRead) {
+    option.value = option.textContent = "Read";
+    optionTwo.value = optionTwo.textContent = "Not read";
+  } else {
+    option.value = option.textContent = "Not read";
+    optionTwo.value = optionTwo.textContent = "Read";
+  }
+
+  const titleTextCell = document.createTextNode(newBook.title);
+  const authorTextCell = document.createTextNode(newBook.author);
+  titleCell.appendChild(titleTextCell);
+  authorCell.appendChild(authorTextCell);
+  select.appendChild(option);
+  select.appendChild(optionTwo);
+  row.appendChild(titleCell);
+  row.appendChild(authorCell);
+  row.appendChild(select);
+  bookList.appendChild(row);
+}
